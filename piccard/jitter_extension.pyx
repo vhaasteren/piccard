b8c46797633e900187a5efd9e1819235da895098
Rutger van Haasteren <vhaasteren@gmail.com>
2014-11-22 20:40:07 -0800

    The jitter extension pyx file was missing

---
+++ piccard/jitter_extension.pyx
@@ -0,0 +1,61 @@
+import numpy as np
+cimport numpy as np
+from libc.math cimport log
+
+def cython_block_shermor_inv(np.ndarray[np.double_t,ndim=1] t, \
+        np.ndarray[np.double_t,ndim=1] r, \
+        np.ndarray[np.double_t,ndim=1] Nvec, \
+        np.ndarray[np.double_t,ndim=1] Jvec, \
+        np.ndarray[np.double_t,ndim=2] Umat, \
+        int nblocks, int blocksize):
+    """
+    Cythonized Sherman-Woodbury inversion
+    """
+    cdef unsigned int lenr = len(r)
+    cdef unsigned int cc, ii, jj
+    cdef np.ndarray[np.double_t,ndim=1] Nblock = np.random.randn(blocksize)
+    cdef np.ndarray[np.double_t,ndim=1] rblock = np.random.randn(blocksize)
+    cdef np.ndarray[np.double_t,ndim=1] ni = np.zeros(nblocks, 'd')
+    cdef double Jldet=0.0, ji, beta, xNx=0.0, temp
+
+    for cc in range(nblocks):
+        ji = 1.0 / Jvec[cc]
+        ni = 1.0 / Nblock
+        beta = 1.0 / (np.sum(ni) + ji)
+
+        temp = 0.0
+        for ii in range(blocksize):
+            Jldet += log(Nblock[ii])
+            xNx += rblock[ii]*rblock[ii]*ni[ii]
+            temp += rblock[ii]*ni[ii]
+        Jldet += log(Jvec[cc]) - log(beta)
+        xNx += beta * (temp**2)
+
+    return Jldet, xNx
+
+
+def block_shermor_inv(t, r, Nvec, Jvec, Umat):
+    Nir = np.zeros(len(r))
+    Jldet = 0.0
+    xNx = 0.0
+    
+    nobs = Umat.shape[0]
+    nbs = Umat.shape[1]
+    nbl = nobs / nbs
+    
+    for cc, col in enumerate(Umat.T):
+        #u = (col == 1.0)
+        Nblock = Nvec[cc*nbl:cc*(nbl+1)]
+        rblock = r[cc*nbl:cc*(nbl+1)]
+        
+        ji = 1.0 / Jvec[cc]
+        ni = 1.0 / Nblock
+        beta = 1.0 / (np.sum(ni) + ji)
+
+        #xNx += np.sum(rblock**2 / Nblock) - beta*np.sum(rblock*Nblock)**2
+        xNx += np.dot(rblock, rblock * ni) - beta * np.dot(rblock, ni)**2
+        
+        #Jldet += np.sum(np.log(Nblock)) + np.log(Jvec[cc]) - np.log(beta)
+        Jldet += np.einsum('i->', np.log(Nblock)) + np.log(Jvec[cc]) - np.log(beta)
+        
+    return Jldet, xNx
