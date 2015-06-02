0dcc9209389acf1b00c67917e84b72333702115f
Rutger van Haasteren <vhaasteren@gmail.com>
2015-06-02 14:19:22 -0600

    Added option to use metronomes

--- piccard/datafile.py
+++ piccard/datafile.py
@@ -54,20 +54,20 @@ except ImportError:
     ephem = None
 
 
-"""
-The DataFile class is the class that supports the HDF5 file format. All HDF5
-file interactions happen in this class.
-"""
 class DataFile(object):
+    """
+    The DataFile class is the class that supports the HDF5 file format. All HDF5
+    file interactions happen in this class.
+    """
     filename = None
     h5file = None
 
-    """
-    Initialise the structure.
-
-    @param filename:    name of the HDF5 file
-    """
     def __init__(self, filename=None):
+        """
+        Initialise the structure.
+
+        @param filename:    name of the HDF5 file
+        """
         # Open the hdf5 file?
         self.filename = filename
 
@@ -75,10 +75,10 @@ class DataFile(object):
         # Delete the instance, and close the hdf5 file?
         pass
 
-    """
-    Return a list of pulsars present in the HDF5 file
-    """
     def getPulsarList(self):
+        """
+        Return a list of pulsars present in the HDF5 file
+        """
         # 'r' means: read file, must exist
         self.h5file = h5.File(self.filename, 'r')
         psrlist = list(self.h5file)
@@ -86,15 +86,15 @@ class DataFile(object):
 
         return psrlist
 
-    """
-    Obtain the hdf5 group of pulsar psrname, create it if it does not exist. If
-    delete is toggled, delete the content first. This function assumes the hdf5
-    file is opened (not checked)
-
-    @param psrname: The name of the pulsar
-    @param delete:  If True, the pulsar group will be emptied before use
-    """
     def getPulsarGroup(self, psrname, delete=False):
+        """
+        Obtain the hdf5 group of pulsar psrname, create it if it does not exist. If
+        delete is toggled, delete the content first. This function assumes the hdf5
+        file is opened (not checked)
+
+        @param psrname: The name of the pulsar
+        @param delete:  If True, the pulsar group will be emptied before use
+        """
         # datagroup = h5file.require_group('Data')
 
         if psrname in self.h5file and delete:
@@ -104,16 +104,16 @@ class DataFile(object):
 
         return pulsarGroup
 
-    """
-    Add data to a specific pulsar. Here the hdf5 file is opened, and the right
-    group is selected
-
-    @param psrname:     The name of the pulsar
-    @param field:       The name of the field we will be writing to
-    @param data:        The data we are writing to the field
-    @param overwrite:   Whether the data should be overwritten if it exists
-    """
     def addData(self, psrname, field, data, overwrite=True):
+        """
+        Add data to a specific pulsar. Here the hdf5 file is opened, and the right
+        group is selected
+
+        @param psrname:     The name of the pulsar
+        @param field:       The name of the field we will be writing to
+        @param data:        The data we are writing to the field
+        @param overwrite:   Whether the data should be overwritten if it exists
+        """
         if self.filename is None:
             raise RuntimeError, "HDF5 filename not provided"
 
@@ -128,20 +128,20 @@ class DataFile(object):
 
         
 
-    """
-    Read data from a specific pulsar. If the data is not available, the hdf5
-    file is properly closed, and an exception is thrown
-
-    @param psrname:     Name of the pulsar we are reading data from
-    @param field:       Field name of the data we are requestion
-    @param subgroup:    If the data is in a subgroup, get it from there
-    @param dontread:    If set to true, do not actually read anything
-    @param required:    If not required, do not throw an exception, but return
-                        'None'
-    @param isort:       If not None, use this as a slice when reading
-    """
     def getData(self, psrname, field, subgroup=None, \
             dontread=False, required=True, isort=None):
+        """
+        Read data from a specific pulsar. If the data is not available, the hdf5
+        file is properly closed, and an exception is thrown
+
+        @param psrname:     Name of the pulsar we are reading data from
+        @param field:       Field name of the data we are requestion
+        @param subgroup:    If the data is in a subgroup, get it from there
+        @param dontread:    If set to true, do not actually read anything
+        @param required:    If not required, do not throw an exception, but return
+                            'None'
+        @param isort:       If not None, use this as a slice when reading
+        """
         # Dontread is useful for readability in the 'readPulsarAuxiliaries
         if dontread:
             return None
@@ -194,14 +194,14 @@ class DataFile(object):
 
         return hasfield
 
-    """
-    Retrieve the shape of a specific dataset
-
-    @param psrname:     Name of the pulsar we are reading data from
-    @param field:       Field name of the data we are requestion
-    @param subgroup:    If the data is in a subgroup, get it from there
-    """
     def getShape(self, psrname, field, subgroup=None):
+        """
+        Retrieve the shape of a specific dataset
+
+        @param psrname:     Name of the pulsar we are reading data from
+        @param field:       Field name of the data we are requestion
+        @param subgroup:    If the data is in a subgroup, get it from there
+        """
         if self.filename is None:
             raise RuntimeError, "HDF5 filename not provided"
 
@@ -227,16 +227,16 @@ class DataFile(object):
         return shape
 
 
-    """
-    (Over)write a field of data for a specific pulsar/group. Data group is
-    required, instead of a name.
-
-    @param dataGroup:   Group object
-    @param field:       Name of field that we are writing to
-    @param data:        The data that needs to be written
-    @param overwrite:   If True, data will be overwritten (default True)
-    """
     def writeData(self, dataGroup, field, data, overwrite=True):
+        """
+        (Over)write a field of data for a specific pulsar/group. Data group is
+        required, instead of a name.
+
+        @param dataGroup:   Group object
+        @param field:       Name of field that we are writing to
+        @param data:        The data that needs to be written
+        @param overwrite:   If True, data will be overwritten (default True)
+        """
         if field in dataGroup and overwrite:
             del dataGroup[field]
 
@@ -247,23 +247,23 @@ class DataFile(object):
                 print("WARNING: h5py too old to support empty arrays: {0}".
                         format(field))
 
-    """
-    Add a pulsar to the HDF5 file, given a tempo2 par and tim file. No extra
-    model matrices and auxiliary variables are added to the HDF5 file. This
-    function interacts with the libstempo Python interface to Tempo2
-
-    @param parfile:     Name of tempo2 parfile
-    @param timfile:     Name of tempo2 timfile
-    @param iterations:  Number of fitting iterations to do before writing
-    @param mode:        Can be replace/overwrite/new. Replace first deletes the
-                        entire pulsar group. Overwrite overwrites all data, but
-                        does not delete the auxiliary fields. New requires the
-                        pulsar not to exist, and throws an exception otherwise.
-    @param dofit:       Whether or not to do a fit at first (default: False)
-    @param maxobs:      Maximum number of observations (if None, use standard)
-    """
     def addTempoPulsar(self, parfile, timfile, iterations=1, mode='replace', \
             dofit = False, maxobs=20000):
+        """
+        Add a pulsar to the HDF5 file, given a tempo2 par and tim file. No extra
+        model matrices and auxiliary variables are added to the HDF5 file. This
+        function interacts with the libstempo Python interface to Tempo2
+
+        @param parfile:     Name of tempo2 parfile
+        @param timfile:     Name of tempo2 timfile
+        @param iterations:  Number of fitting iterations to do before writing
+        @param mode:        Can be replace/overwrite/new. Replace first deletes the
+                            entire pulsar group. Overwrite overwrites all data, but
+                            does not delete the auxiliary fields. New requires the
+                            pulsar not to exist, and throws an exception otherwise.
+        @param dofit:       Whether or not to do a fit at first (default: False)
+        @param maxobs:      Maximum number of observations (if None, use standard)
+        """
         # Check whether the two files exist
         if not os.path.isfile(parfile) or not os.path.isfile(timfile):
             raise IOError, "Cannot find parfile (%s) or timfile (%s)!" % (parfile, timfile)
@@ -413,18 +413,158 @@ class DataFile(object):
         self.h5file.close()
         self.h5file = None
 
-    """
-    Add pulsars to the HDF5 file, given the name of another hdf5 file and a list
-    of pulsars. The data structures will be directly copied from the source file
-    to this one.
+    def addMetronome(self, name, F0, toafile, mode='replace'):
+        """
+        Add a metronome to the HDF5 file, given a file with TOAs. It is treated
+        as a pulsar henceforth, but it has a simpler timing model.  No extra
+        model matrices and auxiliary variables are added to the HDF5 file.
 
-    @param h5file:  The name of the other HDF5 file from which we will be adding
-    @param pulsars: Which pulsars to read ('all' = all, otherwise provide a
-                    list: ['J0030+0451', 'J0437-4715', ...])
-                    WARNING: duplicates are _not_ checked for.
-    @param mode:    Whether to just add, or overwrite (add/replace)
-    """
+        @param name:        Name of this metronome
+        @param F0:          Initially determined pulse period
+        @param toafile:     Name of file with TOAs
+        @param mode:        Can be replace/overwrite/new. Replace first deletes the
+                            entire pulsar group. Overwrite overwrites all data, but
+                            does not delete the auxiliary fields. New requires the
+                            pulsar not to exist, and throws an exception otherwise.
+        """
+        # Check whether the two files exist
+        if not os.path.isfile(toafile):
+            raise IOError, "Cannot find toafile (%s)!" % (toafile)
+
+        if self.filename is None:
+            raise RuntimeError, "HDF5 filename not provided"
+
+        # Parse the default write behaviour
+        deletepsr = False
+        if mode == 'replace':
+            deletepsr = True
+        overwrite = False
+        if mode == 'overwrite':
+            overwrite = True
+
+        # 'a' means: read/write if exists, create otherwise
+        self.h5file = h5.File(self.filename, 'a')
+
+        # Load the TOA data, and save it in the HDF5 file
+        mdata = np.loadtxt(toafile)
+        with open(toafile, 'r') as content_file:
+            timfile_content = content_file.read()
+        parfile_content = "METRONOME-{0}".format(name)
+
+        # Get the pulsar group
+        psrGroup = self.getPulsarGroup(name, delete=deletepsr)
+
+        # Save the par-file and the tim-file to the HDF5 file
+        self.writeData(psrGroup, 'parfile', parfile_content, overwrite=overwrite)
+        self.writeData(psrGroup, 'timfile', timfile_content, overwrite=overwrite)
+
+        # Set the raw data
+        toas = mdata[:,0]
+        toaerrs = mdata[:,1]
+        freqs = np.ones_like(toas) * 1000       # 1 kHz per default
+        raj = 1.0
+        decj = 1.0
+
+        # Determine the pulse number of the first observation
+        pn_0 = int(toas[0] * F0 + 0.5)
+        rphase_0 = toas[0] - pn_0 / F0      # Residual phase first obs
+        
+        # Expected TOAs, parameters, and residuals now are:
+        toas_exp = rphase_0 + np.arange(pn_0, pn_0+len(toas)) / F0
+        residuals_0 = toas - toas_exp
+        pars_0 = np.array([rphase_0, F0])
+
+        # Construct the design matrix
+        M = np.zeros((len(toas), 2))
+        M[:,0] = 1.0
+        M[:,1] = -toas / F0
+
+        # Perform a linear least-squares fit
+        # deltapars = (M.T N^{-1} M)^{-1} M.T N^{-1} residuals
+        Nvec = toaerrs**2
+        MNM = np.dot(M.T / Nvec, M)
+        cf = sl.cho_factor(MNM)
+        dpars = sl.cho_solve(cf, np.dot(M.T, residuals_0 / Nvec))
+        Sigma = sl.cho_sovle(cf, np.eye(len(MNM)))
+
+        # Update the parameters and the residuals
+        pars = pars_0 + dpars
+        parerrs = np.sqrt(np.diag(Sigma))
+        residuals = residuals_0 - np.dot(M, dpars)
+
+        self.writeData(psrGroup, 'TOAs', toas, overwrite=overwrite)    # Seconds
+        self.writeData(psrGroup, 'prefitRes', residuals_0, overwrite=overwrite)  # Seconds
+        self.writeData(psrGroup, 'postfitRes', residuals, overwrite=overwrite)  # Seconds
+        self.writeData(psrGroup, 'toaErr', toaerrs, overwrite=overwrite)    # Seconds
+        self.writeData(psrGroup, 'freq', freqs, overwrite=overwrite)    # MHz
+        self.writeData(psrGroup, 'designmatrix', M, overwrite=overwrite)
+        self.writeData(psrGroup, 'raj', raj, overwrite=overwrite)
+        self.writeData(psrGroup, 'decj', decj, overwrite=overwrite)
+
+        # Now obtain and write the timing model parameters
+        tmpname = ['Offset', 'F0']
+        tmpvalpre = pars_0
+        tmpvalpost = pars
+        tmperrpre = parerrs
+        tmperrpost = parerrs
+
+        self.writeData(psrGroup, 'tmp_name', tmpname, overwrite=overwrite)          # TMP name
+        self.writeData(psrGroup, 'tmp_valpre', tmpvalpre, overwrite=overwrite)      # TMP pre-fit value
+        self.writeData(psrGroup, 'tmp_valpost', tmpvalpost, overwrite=overwrite)    # TMP post-fit value
+        self.writeData(psrGroup, 'tmp_errpre', tmperrpre, overwrite=overwrite)      # TMP pre-fit error
+        self.writeData(psrGroup, 'tmp_errpost', tmperrpost, overwrite=overwrite)    # TMP post-fit error
+
+        # Get the flag group for this pulsar. Create if not there
+        flagGroup = psrGroup.require_group('Flags')
+
+        # Obtain the unique flags in this dataset, and write to file
+        uflags = ['telID']
+        for flagid in uflags:
+            flagtel = ['metronome'] * len(toas)
+            self.writeData(flagGroup, flagid, np.array(flagtel), overwrite=overwrite)
+
+        if not "efacequad" in flagGroup:
+            # Check if the sys-flag is present in this set. If it is, add an
+            # efacequad flag with pulsarname+content of the sys-flag. If it
+            # isn't, check for a be-flag and try the same. Otherwise, add an
+            # efacequad flag with the pulsar name as it's elements.
+            efacequad = []
+            nobs = len(toas)
+            pulsarname = map(str, [name] * nobs)
+
+            if "group" in flagGroup:
+                efacequad = map('-'.join, zip(pulsarname, flagGroup['group']))
+            elif "f" in flagGroup:
+                efacequad = map('-'.join, zip(pulsarname, flagGroup['f']))
+            elif "sys" in flagGroup:
+                efacequad = map('-'.join, zip(pulsarname, flagGroup['sys']))
+            else:
+                efacequad = pulsarname
+
+            self.writeData(flagGroup, "efacequad", efacequad, overwrite=overwrite)
+
+        if not "pulsarname" in flagGroup:
+            nobs = len(toas)
+            pulsarname = map(str, [name] * nobs)
+            self.writeData(flagGroup, "pulsarname", pulsarname, overwrite=overwrite)
+
+        # Close the HDF5 file
+        self.h5file.close()
+        self.h5file = None
+
+
     def addH5Pulsar(self, h5file, pulsars='all', mode='add'):
+        """
+        Add pulsars to the HDF5 file, given the name of another hdf5 file and a list
+        of pulsars. The data structures will be directly copied from the source file
+        to this one.
+
+        @param h5file:  The name of the other HDF5 file from which we will be adding
+        @param pulsars: Which pulsars to read ('all' = all, otherwise provide a
+                        list: ['J0030+0451', 'J0437-4715', ...])
+                        WARNING: duplicates are _not_ checked for.
+        @param mode:    Whether to just add, or overwrite (add/replace)
+        """
         # 'a' means: read/write if exists, create otherwise, 'r' means read
         sourceh5 = h5.File(h5file, 'r')
         self.h5file = h5.File(self.filename, 'a')
@@ -469,21 +609,21 @@ class DataFile(object):
         self.h5file.close()
         sourceh5.close()
 
-    """
-    Read the basic quantities of a pulsar from an HDF5 file into a ptaPulsar
-    object. No extra model matrices and auxiliary variables are read from the
-    HDF5 file, not even residuals. If any field is not present in the HDF5 file,
-    an IOError exception is raised
-
-    @param psr:     The ptaPulsar object we need to fill with data
-    @param psrname: The name of the pulsar to be read from the HDF5 file
-
-    TODO: The HDF5 file is opened and closed every call of 'getData'. That seems
-          kind of inefficient
-    TODO: This function should not 'know' about the psr object. Reading/writing
-          of these quantities should take place outside of this
-    """
     def readPulsar(self, psr, psrname):
+        """
+        Read the basic quantities of a pulsar from an HDF5 file into a ptaPulsar
+        object. No extra model matrices and auxiliary variables are read from the
+        HDF5 file, not even residuals. If any field is not present in the HDF5 file,
+        an IOError exception is raised
+
+        @param psr:     The ptaPulsar object we need to fill with data
+        @param psrname: The name of the pulsar to be read from the HDF5 file
+
+        TODO: The HDF5 file is opened and closed every call of 'getData'. That seems
+              kind of inefficient
+        TODO: This function should not 'know' about the psr object. Reading/writing
+              of these quantities should take place outside of this
+        """
         print("WARNING: readPulsar has been deprecated!")
         psr.name = psrname
 
